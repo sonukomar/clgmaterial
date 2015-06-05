a5d46c55f88f441642fcd4d8c01cc255a916984f
sonukomar <enggsonukomar@gmail.com>
2015-06-05 13:32:31 +0530

    changed urls file for the static files

--- clgmaterial/urls.py
+++ clgmaterial/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from home import views
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.home, name='home')
 ]
+
+if not settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
