6ea0e82293a42d002afb796f03836adce8477785
sonukomar <enggsonukomar@gmail.com>
2015-06-05 13:01:19 +0530

    Updated setttings.py

--- clgmaterial/settings.py
+++ clgmaterial/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'p$*j*va2b)@m9p52sso@+vmpt85e=3l%%nxhh(*7n(tw*s%s_4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -100,6 +100,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
+
+STATIC_ROOT = 'staticfiles'
 
 STATIC_URL = '/static/'
 
