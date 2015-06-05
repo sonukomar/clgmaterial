2edbbd68f35f45742fb2979d2054353dbfbb5a54
sonukomar <enggsonukomar@gmail.com>
2015-06-05 13:06:28 +0530

    Updated setttings.py

--- clgmaterial/settings.py
+++ clgmaterial/settings.py
@@ -102,8 +102,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_ROOT = 'staticfiles'
+if debug:
+    STATIC_URL = '/static/'
+else:
+    STATIC_URL = 'https://clgmaterial.herokuapp.com'+'/static/'    
 
-STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     
